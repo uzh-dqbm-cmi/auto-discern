b037d9ea8f4c89a89e04474594ea06248fc73ee7
lokijuhy <laura.kinkead@uzh.ch>
2019-07-26 16:16:04 +0200

    added result tracking to sacred

--- sacred_experiments/first_experiment.py
+++ sacred_experiments/first_experiment.py
@@ -2,6 +2,7 @@ from autodiscern import DataManager, model
 from autodiscern.experiment import PartitionedExperiment
 from autodiscern.experiments.DocExperiment import DocLevelModelRun
 import pandas as pd
+from pathlib import Path
 from sacred import Experiment
 from sklearn.ensemble import RandomForestClassifier
 from sacred.observers import MongoObserver
@@ -19,7 +20,7 @@ def my_config():
     discern_path = "~/switchdrive/Institution/discern"
     cache_file = '2019-07-24_15-43-08_bd60a7f_with_mm_and_ner_ammendments.pkl'
     # cache_file = '2019-07-24_13-40-50_166c23e_test_mm_and_ner_ammendments_on_subset_30'
-    important_qs = [4]  # , 5, 9, 10, 11]
+    important_qs = [4, 5, 9, 10, 11]
     
     n_estimators_default = 500
     min_samples_leaf_default = 5
@@ -77,8 +78,29 @@ def my_main(discern_path, cache_file, important_qs, model_class, hyperparams, mo
     # view F1 scores
     all_q_f1 = pd.concat([question_models[q].show_evaluation(metric='f1') for q in question_models], axis=0)
     print(all_q_f1)
+    all_f1_path = Path(dm.data_path, 'results/f1.txt')
+    with open(all_f1_path, 'w') as f:
+        f.write(all_q_f1.to_string())
+    ex.add_artifact(all_f1_path)
+
+    # try saving partition accuracies as metric entries?
+
+    # try saving individual f1 scores as metrics
+    for q in all_q_f1.index:
+        for col_name in all_q_f1.columns:
+            if 'partition' in col_name:
+                partiton_number = int(col_name.split(' ')[1])
+                value = round(all_q_f1.loc[q, col_name], 3)
+                ex.log_scalar("{}_f1".format(q), value, partiton_number)
+
+    # save the result
+    ex.result = all_q_f1['median'].to_string()
 
     # view feature importances
     for q in question_models:
         print("{}: {}".format(q, model.questions[int(q.split('q')[1])]))
         print(question_models[q].show_feature_importances().head(10))
+        feature_path = Path(dm.data_path, 'results/feature_importances_{}.txt'.format(q))
+        with open(feature_path, 'w') as f:
+            f.write(question_models[q].show_feature_importances().head(20).to_string())
+        ex.add_artifact(feature_path)
