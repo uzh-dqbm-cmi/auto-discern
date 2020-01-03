c46fd71f09d44121b533cdd7f6965d290d534a8d
lokijuhy <laura.kinkead@uzh.ch>
2020-01-03 11:27:09 +0100

    version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='autodiscern',
-      version='0.0.1',
+      version='0.0.2',
       description='',
       url='https://github.com/CMI-UZH/auto-discern',
       packages=['autodiscern', 'autodiscern.experiments', 'autodiscern.predictors'],
